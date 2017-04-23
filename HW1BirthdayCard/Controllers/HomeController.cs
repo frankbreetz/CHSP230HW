fb5f4f2b0acf91a944d44ac97e730d838515fa91
frankbreetz <franckbreetz@gmail.com>
2017-04-23 16:12:28 -0400

    This is the first homework

---
+++ HW1BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW1BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult BirthdayMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult BirthdayMessage(Models.BirthdayWish birthdayWish)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks",birthdayWish );
+            }
+            else
+            {
+                return View();
+            }
+            //return View($"Thanks", );
+        }
+    }
+}
---
+++ HW1BirthdayCard/Models/BirthdayWish.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HW1BirthdayCard.Models
+{
+    public class BirthdayWish
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string From { get; set; }
+        [Required(ErrorMessage = "Please enter the recipients name")]
+        public string To { get; set; }
+        [Required(ErrorMessage = "Please enter a special message for you friend")]
+        public string Message { get; set; }
+    }
+}
