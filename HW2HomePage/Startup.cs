05c84150ab27682799964bf431f628a6032bbb04
frankbreetz <franckbreetz@gmail.com>
2017-04-23 16:47:41 -0400

    This is the second homework

---
+++ HW2HomePage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HW2HomePage.Startup))]
+namespace HW2HomePage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
